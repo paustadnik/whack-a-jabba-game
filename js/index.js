e7e3808beadf9baa2019171cb5247c97be6856ef
Paulina Stadnik <paulina.stadnik@rover.com>
2021-12-28 15:03:06 +0100

    Added timer

--- js/index.js
+++ js/index.js
@@ -7,19 +7,43 @@ const score = document.querySelector(".score > span")
 const scoreContainer = document.querySelector(".scoringArea")
 const jabbas = document.querySelectorAll(".jabba")
 const startBtn = document.querySelector('.startBtn')
+const timerArea = document.querySelector('.scoreTimeContainer')
+const timer = document.querySelector('.timer')
 
 let points = 0;
+let currentTime = 30
+timer.innerHTML = `00:${currentTime}`
+
 
 function hideGame() {
     playField.style.visibility = 'hidden'
     scoreContainer.style.visibility = "hidden"
+    timerArea.style.visibility = "hidden"
     points = 0
 }
 
 function startGame() {
     playField.style.visibility = 'visible'
     scoreContainer.style.visibility = 'visible'
+    timerArea.style.visibility = "visible"
     startBtn.style.visibility = 'hidden'
+}
+
+function countdown() {
+    let timeInterval = setInterval(() => {
+        console.log(currentTime)
+        currentTime -= 1
+        if (currentTime < 10) {
+            timer.innerHTML = `00:0${currentTime}`
+            timer.style.color = 'red'
+        }
+        else {
+            timer.innerHTML = `00:${currentTime}`
+        }
+        if (currentTime === 0) {
+            clearInterval(timeInterval)
+        }
+    }, 1000);
 }
 
 window.onload = () => {
@@ -27,7 +51,8 @@ window.onload = () => {
 }
 
 startBtn.onclick = () => {
-    startGame() 
+    startGame()
+    countdown()
 
     const interval = setInterval(() => {
         const randIndex = Math.floor(Math.random() * hidingObjects.length)
@@ -35,7 +60,7 @@ startBtn.onclick = () => {
             object.classList.remove('active')
         });
         hidingObjects[randIndex].classList.add('active')
-    }, 1200)
+    }, 1100)
 
     hidingObjects.forEach((element) => {
         element.onclick = () => {
